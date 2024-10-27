b1d510d4d5f95feebcd35346a6a8596b361e3716
splexas <official.fluk3@gmail.com>
2024-10-27 19:36:04 +0200

    style: endif specifying

--- include/websockets/handshake.h
+++ include/websockets/handshake.h
@@ -13,4 +13,4 @@ struct header_entry *ws_parse_handshake(const char *data, const int data_len);
 void ws_free_handshake(struct header_entry *head);
 struct header_entry *ws_header_get(struct header_entry *head, const char *key);
 
-#endif
+#endif // _WEBSOCKETS_HANDSHAKE_H
